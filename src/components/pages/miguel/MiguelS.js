fd396a72658bb53bda3bda3fe348a68209b744d7
MiggyL <116647018+MiggyL@users.noreply.github.com>
2024-04-03 22:11:59 +0800

    Add files via upload

---
+++ src/components/pages/miguel/MiguelS.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './miguelstyles.css'; // Import your CSS file
+
+function MiguelS() {
+    return (
+        <div className="centered-name">
+            Lacanienta, Miguel M. SN: 2019130430
+        </div>
+    );
+}
+
+export default MiguelS;
