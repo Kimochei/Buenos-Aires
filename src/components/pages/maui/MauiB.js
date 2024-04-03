7200e712d39b43467204c0b6d93655457bea43f0
ResearcherA-7 <lakremaxd@gmail.com>
2024-04-03 22:53:19 +0800

    added own page
    cloned repository, added my own page then commit back to main

---
+++ src/components/pages/maui/MauiB.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './mauistyles.css';
+
+function MauiB() {
+    return (
+        <div className="centered-name">
+            Battung, Maui Michelin V. SN: 2021106464
+        </div>
+    );
+}
+
+export default MauiB;
