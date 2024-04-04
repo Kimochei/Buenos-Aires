c0b804ba865de01ee1695b49ac59f674639536b2
Michi <164274507+JerwieCruz@users.noreply.github.com>
2024-04-04 19:26:46 +0800

    Add files via upload

---
+++ src/components/pages/jerwie/JerwieC.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './jerwiestyles.css';
+
+function JerwieC() {
+  return (
+    <div className="centered-name">
+        Cruz, Jerwie Nathan A. SN: 2021105129
+        <Outlet />
+
+    </div>
+  );
+}
+
+export default JerwieC;
