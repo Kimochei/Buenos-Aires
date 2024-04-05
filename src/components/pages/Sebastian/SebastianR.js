0b0804a8459b8865dc4cef87a99ddf389a77d6a7
Seb131415 <155604141+Seb131415@users.noreply.github.com>
2024-04-05 14:55:16 +0800

    Add files via upload

---
+++ src/components/pages/Sebastian/SebastianR.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './SebastianR.css'; // Import your CSS file
+
+function SebastianR() {
+    return (
+        <div className="centered-name">
+            Ramirez, Sebastian Miguel Y. SN: 2021105401
+        </div>
+    );
+}
+
+export default SebastianR;
